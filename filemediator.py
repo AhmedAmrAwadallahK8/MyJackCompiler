e38f0d2f55e72115093e01883d620c30c07305ae
AhmedAmrAwadallahK8 <68250969+AhmedAmrAwadallahK8@users.noreply.github.com>
2021-10-15 12:38:02 -0700

    Added documentation for extract_file

--- filemediator.py
+++ filemediator.py
@@ -2,12 +2,24 @@ import sys
 import os
 
 
-def extract(file_name):
+def extract_file(file_name):
+    """Takes a file and returns a list of its contents
+
+        Parameters:
+        file_name (String): Name of file
+
+        Returns:
+        list: List made up of all the lines of text within the file
+
+    """
     file_contents = []
     with open(file_name, 'r') as f:
         for l in f:
             file_contents.append(l)
     return file_contents
+
+def extract_folder(folder_name):
+    return
 
 
 output = extract('testtext')
