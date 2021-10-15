f30fedf0e01509833526064caa9438d637c30a10
AhmedAmrAwadallahK8 <68250969+AhmedAmrAwadallahK8@users.noreply.github.com>
2021-10-15 12:58:13 -0700

    Added extract_folder

--- filemediator.py
+++ filemediator.py
@@ -3,27 +3,37 @@ import os
 
 
 def extract_file(file_name):
-    """Takes a file and returns a list of its contents
+    """ Takes a file and returns a list of its contents
 
-        Parameters:
-        file_name (String): Name of file
-
-        Returns:
-        list: List made up of all the lines of text within the file
-
+    :param file_name: (String) Name of file
+    :return: (List) made up of all the lines of text within the file
     """
     file_contents = []
     with open(file_name, 'r') as f:
-        for l in f:
-            file_contents.append(l)
+        for line in f:
+            file_contents.append(line)
     return file_contents
 
+
 def extract_folder(folder_name):
-    return
+    """
+
+    :param folder_name: (String) Name of folder
+    :return: (Dictionary) Folders Path and List of files
+    """
+    origin_path = os.getcwd()
+    folder_path = origin_path + '/' + folder_name
+    os.chdir(folder_path)
+    file_list = os.listdir()
+    return {"path": folder_path, "files": file_list}
 
 
-output = extract('testtext')
+output = extract_file('testtext.txt')
 print(output)
+print(os.getcwd())
+output = extract_folder('testdirec')
+print(output)
+print(os.getcwd())
 
 
 
