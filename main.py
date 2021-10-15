a0ad779c3d60c7f8881384a4a8f0a43338824147
AhmedAmrAwadallahK8 <68250969+AhmedAmrAwadallahK8@users.noreply.github.com>
2021-10-15 13:19:01 -0700

    Added folder_test

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
 import sys
+import os
 import filemediator as fm
 
 
+def folder_test(file_name):
+    """
+
+    :param file_name: (String) Name of file
+    :return: (Boolean) Returns true if the file is a directory else false
+    """
+    origin_path = os.getcwd()
+    file_path = origin_path + '/' + file_name
+    return os.path.isdir(file_path)
+
+
+def main(sys_input):
+
+    return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#input = sys.argv[1]
+#main(input)
 
