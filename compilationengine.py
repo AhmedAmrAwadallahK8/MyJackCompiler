d4a872b6ecd15022ff92b17361802b69adb6971f
AhmedAmrAwadallahK8 <68250969+AhmedAmrAwadallahK8@users.noreply.github.com>
2021-10-18 15:40:07 -0700

    Partially implemented compile_while_statement and updated xml_snippet to help deal with redundant code

--- compilationengine.py
+++ compilationengine.py
@@ -19,39 +19,38 @@ class CompilationEngine:
         else:
             print('No more tokens left')
 
-    def xml_snippet(self):
+    def xml_snippet_ll_1(self):
         xml_code = '\t' * self.tab_num
         xml_code += '<' + self.current_token.get_variety() + '> '
         xml_code += self.current_token.get_val()
         xml_code += ' </' + self.current_token.get_variety() + '>\n'
+        self.next_token()
         return xml_code
 
     def compile_class_var_dec(self):
+        """Responsible for parsing a class variable declaration
+
+        :return: (String) XML representation
+        """
         self.tab_num += 1
         out_xml = '\t'*self.tab_num
         out_xml += '<classVarDec>\n'
         if self.current_token.get_val() == 'static' or self.current_token.get_val() == 'field':
             self.tab_num += 1
             # Expect Keyword = static or field
-            out_xml += self.xml_snippet()
-            self.next_token()
+            out_xml += self.xml_snippet_ll_1()
             # Expect a type
-            out_xml += self.xml_snippet()
-            self.next_token()
+            out_xml += self.xml_snippet_ll_1()
             # Expect a variable name
-            out_xml += self.xml_snippet()
-            self.next_token()
+            out_xml += self.xml_snippet_ll_1()
             # Expect ; or expect more variable names
             while self.current_token.get_val() != ';':
                 # Expect a comma
-                out_xml += self.xml_snippet()
-                self.next_token()
+                out_xml += self.xml_snippet_ll_1()
                 # Expect a variable name
-                out_xml += self.xml_snippet()
-                self.next_token()
+                out_xml += self.xml_snippet_ll_1()
             # Expect a ;
-            out_xml += self.xml_snippet()
-            self.next_token()
+            out_xml += self.xml_snippet_ll_1()
             self.tab_num -= 1
         out_xml += '\t' * self.tab_num
         out_xml += '</classVarDec>\n'
@@ -59,36 +58,71 @@ class CompilationEngine:
         return out_xml
 
     def compile_var_dec(self):
+        """Responsible for parsing a variable declaration
+
+        :return: (String) XML representation
+        """
         self.tab_num += 1
         out_xml = '\t' * self.tab_num
         out_xml += '<varDec>\n'
         if self.current_token.get_val() == 'var':
             self.tab_num += 1
             # Expect Keyword = var
-            out_xml += self.xml_snippet()
-            self.next_token()
+            out_xml += self.xml_snippet_ll_1()
             # Expect a type
-            out_xml += self.xml_snippet()
-            self.next_token()
+            out_xml += self.xml_snippet_ll_1()
             # Expect a variable name
-            out_xml += self.xml_snippet()
-            self.next_token()
+            out_xml += self.xml_snippet_ll_1()
             # Expect ; or expect more variable names
             while self.current_token.get_val() != ';':
                 # Expect a comma
-                out_xml += self.xml_snippet()
-                self.next_token()
+                out_xml += self.xml_snippet_ll_1()
                 # Expect a variable name
-                out_xml += self.xml_snippet()
-                self.next_token()
+                out_xml += self.xml_snippet_ll_1()
             # Expect a ;
-            out_xml += self.xml_snippet()
-            self.next_token()
+            out_xml += self.xml_snippet_ll_1()
             self.tab_num -= 1
         out_xml += '\t' * self.tab_num
         out_xml += '</varDec>\n'
         self.tab_num -= 1
         return out_xml
+
+    def compile_expression(self):
+        return "Incomplete method"
+
+    def compile_statements(self):
+        return "Incomplete method"
+
+    def compile_while_statement(self):
+        self.tab_num += 1
+        out_xml = '\t'*self.tab_num
+        out_xml += '<whileStatement>\n'
+        self.tab_num += 1
+        # Expect a While
+        out_xml += self.xml_snippet_ll_1()
+        # Expect a (
+        out_xml += self.xml_snippet_ll_1()
+        # Expect a expression
+        out_xml += self.compile_expression()
+        # Expect a )
+        out_xml += self.xml_snippet_ll_1()
+        # Expect a {
+        out_xml += self.xml_snippet_ll_1()
+        # Expect statements
+        out_xml += self.compile_statements()
+        # Lastly expect a }
+        out_xml += self.xml_snippet_ll_1()
+        self.tab_num -= 1
+        out_xml += '\t'*self.tab_num
+        out_xml += '</compileWhile>\n'
+        return out_xml
+
+
+
+
+
+
+
 
 
 
