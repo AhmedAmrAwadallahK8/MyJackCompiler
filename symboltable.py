5bd91b420d4b8de71a80458dbdfc7c1588820713
AhmedAmrAwadallahK8 <68250969+AhmedAmrAwadallahK8@users.noreply.github.com>
2021-10-22 17:03:39 -0700

    Implemented the rest of SymbolTable, kind_of remains incomplete

--- symboltable.py
+++ symboltable.py
@@ -2,15 +2,18 @@
 class SymbolTable:
     def __init__(self, class_table=None):
         self.table = {}
-        self.kind_count = {'local': 0, 'argument': 0, 'this': 0, 'that': 0, 'static': 0}
+        self.kind_count = {'static': 0, 'field': 0, 'argument': 0, 'variable': 0}
         self.class_table = class_table
         if class_table is None:
             self.sub_table = SymbolTable(self)
 
     def get_sub(self):
+        """Getter
+
+        :return: (SymbolTable) The sub_table for this SymbolTable
+        """
         return self.sub_table
 
-    # TODO resets sub_table
     def start_subroutine(self):
         """Resets the state of the subroutine symbol table to default
 
@@ -20,7 +23,6 @@ class SymbolTable:
         for k in self.sub_table.kind_count:
             self.sub_table.kind_count[k] = 0
 
-    # TODO defines a new variable and all relevent information
     def define(self, name, j_type, kind):
         """Updates the symbol table with a new variable
 
@@ -32,22 +34,45 @@ class SymbolTable:
         self.table[name] = (j_type, kind, self.kind_count[kind])
         self.kind_count[kind] += 1
 
-    # TODO returs number of given kind of variable in scope
-    def var_count(self):
-        pass
+    def var_count(self, kind):
+        """
 
-    # TODO returns kind of named identifier in current scope
-    def kind_of(self):
-        pass
+        :param kind: VM memory segment kind
+        :return: (int) Number of times the memory segment has been defined in scope
+        """
+        return self.kind_count[kind]
 
-    # TODO returns type of named identifier in current scope
-    def type_of(self):
-        pass
+    # TODO This function is not complete must be able to handle situations where the identifier is unknown in scope
+    def kind_of(self, name):
+        """For a given variable name return its kind
 
-    # TODO returns the index assigned to the named identifier
-    def index_of(self):
-        pass
+        :param name: (String) name of variable
+        :return: (String) the variables's kind
+        """
+        return self.table[name][1]
+
+    def type_of(self, name):
+        """For a given variable name return its type
+
+        :param name: (String) name of variable
+        :return: (String) the variable's type
+        """
+        return self.table[name][0]
+
+    def index_of(self, name):
+        """For a given variable name return the memory index based on its kind
+
+        :param name: (String) name of variable
+        :return: (int) the index in memory allocated for this variable
+        """
+        return self.table[name][2]
 
 
-a = SymbolTable()
+'''a = SymbolTable()
 b = a.get_sub()
+a.define('x', 'int', 'argument')
+print(a.table)
+print(a.var_count('argument'))
+print(a.kind_of('x'))
+print(a.type_of('x'))
+print(a.index_of('x'))'''
