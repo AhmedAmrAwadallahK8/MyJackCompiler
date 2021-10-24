0b495fa4e05ba6720b01295b7e069e365f22dd01
AhmedAmrAwadallahK8 <68250969+AhmedAmrAwadallahK8@users.noreply.github.com>
2021-10-23 19:07:29 -0700

    Finished implemenation of VM translation functions

--- vmwriter.py
+++ vmwriter.py
@@ -13,8 +13,8 @@ def write_push(kind, index):
 def write_pop(kind, index):
     """VM pop command code
 
-    :param kind: Kind of memory segment
-    :param index: Allocated memory index
+    :param kind: (String) Kind of memory segment
+    :param index: (String) Allocated memory index
     :return: (String) VM Translation
     """
     vm_out = 'pop ' + kind + ' ' + index + '\n'
@@ -23,30 +23,96 @@ def write_pop(kind, index):
     ops = ('+', '-', '*', '/', '&gt;', '&lt;', '=', '|', '&amp;')
     unary_ops = ('-', '~')
 def write_arithmetic(op=None, unary_op=None):
+    """VM op code
+
+    :param op: (String) Op symbol
+    :param unary_op: (String) Unary op symbol
+    :return: (String) VM Translation
+    """
+    vm_out = ''
     if op:
-        of
-    pass
+        if op == '+':
+            vm_out += 'add\n'
+        elif op == '-':
+            vm_out += 'sub\n'
+        elif op == '*':
+            vm_out += 'call Math.multiply 2\n'
+        elif op == '/':
+            vm_out += 'call Math.divide 2\n'
+        elif op == '>':
+            vm_out += 'gt\n'
+        elif op == '<':
+            vm_out += 'lt\n'
+        elif op == '=':
+            vm_out += 'eq\n'
+        elif op == '|':
+            vm_out += 'or\n'
+        elif op == '&':
+            vm_out += 'and\n'
+    elif unary_op:
+        if op == '-':
+            vm_out += 'neg\n'
+        elif op == '~':
+            vm_out += 'not\n'
+    return vm_out
 
 
-def write_label():
-    pass
+def write_label(l_name):
+    """VM label code
+
+    :param l_name: (String) Unique label name
+    :return: VM Translation
+    """
+    vm_out = 'label ' + l_name + '\n'
+    return vm_out
 
 
-def write_goto():
-    pass
+def write_goto(tar_label):
+    """VM unconditional goto code
+
+    :param tar_label: (String) Label to jump to
+    :return: (String) VM Translation
+    """
+    vm_out = 'goto ' + tar_label + '\n'
+    return vm_out
 
 
-def write_if():
-    pass
+def write_if(tar_label):
+    """VM conditional goto code
+
+    :param tar_label: (String) Label to jump to
+    :return: (String) VM Translation
+    """
+    vm_out = 'if-goto ' + tar_label + '\n'
+    return vm_out
 
 
-def write_call():
-    pass
+def write_call(subroutine_name, num_args):
+    """VM function call code
+
+    :param subroutine_name: (String) name of subroutine
+    :param num_args: (int) number of arguments the function requires
+    :return: (String) VM Translation
+    """
+    vm_out = 'call ' + subroutine_name + ' ' + num_args + '\n'
+    return vm_out
 
 
-def write_function():
-    pass
+def write_function(subroutine_name, num_vars):
+    """VM function initialization code
+
+    :param subroutine_name: (String) name of subroutine
+    :param num_vars: (int) number of variables the function requires
+    :return: (String) VM Translation
+    """
+    vm_out = 'function ' + subroutine_name + ' ' + num_vars + '\n'
+    return vm_out
 
 
 def write_return():
-    pass
+    """VM return code
+
+    :return: VM Translation
+    """
+    vm_out = 'return\n'
+    return vm_out
